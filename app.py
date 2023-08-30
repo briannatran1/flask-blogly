5882daf184bf27192fd4686b5273773c5cd8e6f7
Christopher Alley <calleyviola@gmail.com>
2023-08-30 15:32:56 -0400

    Add home page and users route

--- app.py
+++ app.py
@@ -16,3 +16,10 @@ connect_db(app)
 
 @app.get('/')
 def show_homepage():
+    """Show a list of all users"""
+    return redirect('/users')
+
+@app.get('/users')
+def show_users():
+    """Show a list of all users"""
+    return render_template('user_listing.html')
