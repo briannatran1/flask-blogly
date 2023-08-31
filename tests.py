e509d0c910911bc34eaf1a81b17e7d92011a7dd1
Christopher Alley <calleyviola@gmail.com>
2023-08-31 17:15:18 -0400

    Add tests for add/edit post forms

--- tests.py
+++ tests.py
@@ -1,4 +1,4 @@
-from models import DEFAULT_IMAGE_URL, User
+from models import DEFAULT_IMAGE_URL, User, Post
 from app import app, db
 from unittest import TestCase
 import os
@@ -40,14 +40,27 @@ class UserViewTestCase(TestCase):
             image_url=None,
         )
 
+
         db.session.add(test_user)
         db.session.commit()
+
 
         # We can hold onto our test_user's id by attaching it to self (which is
         # accessible throughout this test class). This way, we'll be able to
         # rely on this user in our tests without needing to know the numeric
         # value of their id, since it will change each time our tests are run.
         self.user_id = test_user.id
+
+        test_post = Post(
+            title="test1_title",
+            content="test1_content",
+            user_id=self.user_id
+        )
+
+        db.session.add(test_post)
+        db.session.commit()
+
+        self.post_id = test_post.id
 
     def tearDown(self):
         """Clean up any fouled transaction."""
@@ -115,11 +128,54 @@ class UserViewTestCase(TestCase):
         with self.client as c:
             resp = c.post('/users/new',
                           data={"first_name": 'Chris',
-                                "last_name": 'Alley'})
+                                "last_name": 'Alley'},
+                                follow_redirects=True)
             html = resp.get_data(as_text=True)
 
             self.assertEqual(resp.status_code, 200)
             self.assertIn('Chris', html)
+            self.assertIn('Alley', html)
 
-    # TODO: create test for creating a new user w/post
-    # be more specific in test names
+    # def test_handle_new_post_form(self):
+    #     """Tests handling of new post form"""
+    #     with self.client as c:
+    #         resp = c.post(f'/users/{self.user_id}/posts/new',
+    #                       data={
+    #                           'title': 'Very cool post',
+    #                           'content': 'This is some amzing content'
+    #                       },
+    #                       follow_redirects=True)
+    #         html = resp.get_data(as_text=True)
+
+    #         self.assertEqual(resp.status_code, 200)
+    #         self.assertIn('Very cool post', html)
+
+            # resp = c.post(f'/users/{self.user_id}/posts/new',
+            #               data={
+            #                   'title': 'Such wow post',
+            #                   'content': 'This is some amzing content'
+            #               },
+            #               follow_redirects=True)
+            # html = resp.get_data(as_text=True)
+
+            # self.assertEqual(resp.status_code, 200)
+            # self.assertIn('Such wow post', html)
+    def test_show_add_post_form(self):
+        """Tests showing user the add post form"""
+        with self.client as c:
+            resp = c.get(f'/users/{self.user_id}/posts/new')
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('Add Post for', html)
+            self.assertIn('test1_first', html)
+            self.assertIn('test1_last', html)
+
+    def test_show_post_edit_form(self):
+        """Tests showing edit post form for specified post"""
+        with self.client as c:
+            resp = c.get(f'/posts/{self.post_id}/edit')
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('Edit Post', html)
