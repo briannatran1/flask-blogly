5bd0f571890f88756564580a7ebf5a2e023a8cb3
Christopher Alley <calleyviola@gmail.com>
2023-08-31 14:05:41 -0400

    Add post model

--- models.py
+++ models.py
@@ -40,3 +40,36 @@ class User(db.Model):
         nullable=False,
         default=DEFAULT_IMAGE_URL
     )
+
+class Post(db.Model):
+    """Post."""
+
+    __tablename__ = "posts"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+
+    title = db.Column(
+        db.String(50),
+        nullable=False
+    )
+
+    content = db.Column(
+        db.String,
+        nullable=False
+    )
+
+    created_at = db.Column(
+        db.DateTime,
+        nullable=False,
+        default=db.func.now()
+    )
+
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey('users.id'),
+        nullable=False
+    )
